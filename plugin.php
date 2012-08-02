921f28a573ea1372367bf401f7261e948ab81c6a
Marko Heijnen <info@markoheijnen.com>
2012-08-02 16:54:57 +0200

    Add basic structure for the admin page for running tests

--- plugin.php
+++ plugin.php
@@ -19,3 +19,29 @@ include 'image.php';
 
 include 'editors/class-wp-image-editor-gd.php';
 include 'editors/class-wp-image-editor-imagemagick.php';
+
+class WP_Image_Testsuite {
+	function __construct() {
+		add_action( 'admin_menu', array( &$this, 'add_menu_page' ) );
+	}
+
+	function add_menu_page() {
+		add_management_page( 'WP_Image', 'WP_Image', 'manage_options', 'wp-image', array( &$this, 'menu_page' ) );
+	}
+
+	function menu_page() {
+		if ( ! current_user_can('manage_options') )  {
+			wp_die( __('You do not have sufficient permissions to access this page.') );
+			return;
+		}
+
+		echo '<div class="wrap">';
+		echo '<div id="icon-options-general" class="icon32"><br></div>';
+		echo '<h2>WP_Image</h2>';
+
+
+		echo '</div>';
+	}
+}
+
+new WP_Image_Testsuite;
